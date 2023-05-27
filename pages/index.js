8d7222a5ad1548f36a22a22ca4686cf89307ad24
it19046808@my.sllit.lk <janirajayaprabha@gmail.com>
2023-05-27 13:01:03 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import Carousel from '../components/carousel';
